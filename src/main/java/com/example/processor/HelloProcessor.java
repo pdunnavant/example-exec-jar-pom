a63bcfe61fe8849df4726c6a3251f20fc26ef172
Paul Dunnavant <pdunnavant@gmail.com>
2011-08-26 12:39:11 -0700

    added usage of commons-lang library

--- src/main/java/com/example/processor/HelloProcessor.java
+++ src/main/java/com/example/processor/HelloProcessor.java
@@ -1,8 +1,23 @@
 package com.example.processor;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 public class HelloProcessor {
+
+	private static final String[] planets = new String[] {
+		"Mars", "Earth", "Jupiter", "Mercury", "Venus", "Saturn", "Uranus", "Neptune",
+		
+		// I still believe in you, Pluto! :-)
+		"Pluto"
+	};
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		System.out.println("Planets:  " + Arrays.toString(planets));
+		ArrayUtils.reverse(planets);
+		System.out.println("Reversed: " + Arrays.toString(planets));
 	}
 }
